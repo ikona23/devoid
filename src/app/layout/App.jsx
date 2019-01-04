e5858abffd5cd3a8e7a08f39713680f06d89aab0
Peter Kona <atman@Peters-MacBook-Pro.local>
2019-01-04 15:27:22 +1300

    navbar

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-
-
+import { Button } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>devoid</h1>
+      <NavBar/>
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
       </div>
     );
   }
